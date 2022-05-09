3fcd7909f136e3d37a962befffa161180830b70d
yiyuandian <yiyuandian0@sina.cn>
2022-05-10 00:34:21 +0800

    注释去除

--- zip.bat
+++ zip.bat
@@ -2,4 +2,4 @@
 set zip_path="C:\Program Files\7-Zip\"
 set file_path=%~dp0
 
-@REM %zip_path%7z.exe  a -y %file_path%"BOSS.epub" %file_path%"META-INF" %file_path%"OEBPS" %file_path%"mimetype"
+%zip_path%7z.exe  a -y %file_path%"BOSS.epub" %file_path%"META-INF" %file_path%"OEBPS" %file_path%"mimetype"
