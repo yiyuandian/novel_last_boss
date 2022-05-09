52b8c960205cbbe3c6f1a905601b0b2a04862813
yiyuandian <yiyuandian0@sina.cn>
2022-05-10 00:33:58 +0800

    设置为相对路径

--- zip.bat
+++ zip.bat
@@ -1,4 +1,5 @@
 @echo off
 set zip_path="C:\Program Files\7-Zip\"
-set file_path=Y:\git\novel_last_boss\
-%zip_path%7z.exe  a -y %file_path%"BOSS.epub" %file_path%"META-INF" %file_path%"OEBPS" %file_path%"mimetype"
+set file_path=%~dp0
+
+@REM %zip_path%7z.exe  a -y %file_path%"BOSS.epub" %file_path%"META-INF" %file_path%"OEBPS" %file_path%"mimetype"
