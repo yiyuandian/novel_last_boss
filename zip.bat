107cb2592e7230a1ab01b6b5156418c4292ea2e8
yiyuandian <yiyuandian0@sina.cn>
2022-05-10 16:17:50 +0800

    默认成有给7z配置环境变量

--- zip.bat
+++ zip.bat
@@ -1,5 +1,5 @@
 @echo off
-set zip_path="C:\Program Files\7-Zip\"
+@REM set zip_path="C:\Program Files\7-Zip\"
 set file_path=%~dp0
 
-%zip_path%7z.exe  a -y %file_path%"BOSS.epub" %file_path%"META-INF" %file_path%"OEBPS" %file_path%"mimetype"
+7z.exe  a -y %file_path%"BOSS.epub" %file_path%"META-INF" %file_path%"OEBPS" %file_path%"mimetype"
